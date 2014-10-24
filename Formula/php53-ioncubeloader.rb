68fb1981a5432669e29ae7e46651b2481fa0f29f
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-10-24 14:44:51 +1100

    Update ioncubeloader version to 4.7.0

--- Formula/php53-ioncubeloader.rb
+++ Formula/php53-ioncubeloader.rb
@@ -4,11 +4,11 @@ class Php53Ioncubeloader < AbstractPhp53Extension
   init
   homepage 'http://www.ioncube.com/loaders.php'
   if Hardware.is_64_bit?
-    version '4.6.1'
+    version '4.7.0'
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86-64.tar.gz'
     sha1 '87df3d49b07877094dc7e27b833dd1e96863defb'
   else
-    version '4.6.1'
+    version '4.7.0'
     url 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_dar_x86.tar.gz'
     sha1 '70cb4905d5d05fb2d5f3e18aa212d9fcca8a83b9'
   end
